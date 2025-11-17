3039a4c45455167274cf146eecb50bbf5f02c7a1
Elliot Schmit <schmit0709@gmail.com>
2025-11-17 11:23:27 -0500

    fin cours

--- src/scripts/Main.js
+++ src/scripts/Main.js
@@ -1,12 +1,26 @@
 import Icons from './utils/Icons.js';
+import Swiper from 'swiper/bundle';
 
 class Main {
   constructor() {
     this.init();
+    this.initSwiper();
   }
 
   init() {
     Icons.load();
+  }
+
+  initSwiper() {
+    const target = document.querySelector('.js-swiper');
+
+    if (target) {
+      const swiper = new Swiper(target, {
+        pagination: {
+          el: '.swiper-pagination',
+        },
+      });
+    }
   }
 }
 new Main();
